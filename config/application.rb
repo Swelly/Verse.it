c097cb8e0eb7275a9776686f3b6e945d85dc1e47
Wesley Carr <surfswell17@gmail.com>
2013-08-04 18:32:10 -0400

    Switched to psql database from sqlite3

--- config/application.rb
+++ config/application.rb
@@ -47,6 +47,9 @@ module VerseApp
     # like if you have constraints or database-specific column types
     # config.active_record.schema_format = :sql
 
+    # Prevents rails from being precompiled when assets:precompile task is excecuted
+    config.assets.initialize_on_precompile = false
+
     # Enforce whitelist mode for mass assignment.
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
