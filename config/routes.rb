2f6f6dd30fd5cf110287073b5abbdbe2d31a7893
Wesley Carr <surfswell17@gmail.com>
2013-08-05 15:57:24 -0400

    Devise views and omniauth login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 VerseApp::Application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :path => "auth", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
 
   root :to => 'welcome#index'
   get '/about' => 'welcome#about', :as => 'about'
