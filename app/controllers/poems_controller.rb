2b61dfc7daa5a21a5c73cf99976cf8dd2f878f21
alicem <alice.mottola@gmail.com>
2013-08-05 14:41:39 -0400

    Adds basic controller setup

---
+++ app/controllers/poems_controller.rb
@@ -0,0 +1,38 @@
+class PoemsContoller < ApplicationController
+
+  # GET
+  # shows some recent or popular poems?
+  def index
+  end
+
+  # GET
+  # shows a single poem and its metadata
+  def show
+  end
+
+  # GET
+  # shows the form for creating a new poem (i.e. the two-panel dragging display)
+  def new
+  end
+
+  # POST
+  # adds the poem to the database and redirects to show
+  def create
+  end
+
+  # GET
+  # we probably don't need this
+  def edit
+  end
+
+  # PUT
+  # don't need this either
+  def update
+  end
+
+  # DELETE
+  # deletes a poem, redirects to user profile?
+  def destroy
+  end
+
+end
