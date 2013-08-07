7a5ac2b01bc660aa66c0159de817094183ad30ef
alicem <alice.mottola@gmail.com>
2013-08-07 15:13:35 -0400

    Poems now save properly

---
+++ spec/models/poem_spec.rb
@@ -0,0 +1,11 @@
+# spec/models/contact.rb
+require 'spec_helper'
+
+describe Poem do
+
+  it "has a valid factory"
+  it "is invalid without text"
+  it "is invalid without an author"
+  it "is invalid without a source user"
+
+end
