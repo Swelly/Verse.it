135ef4c1a148eb7a09c2ae488fe865b6ea4159e8
alicem <alice.mottola@gmail.com>
2013-08-10 19:41:33 -0400

    Adds provider and uid to users (again)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130810225550) do
+ActiveRecord::Schema.define(:version => 20130810233948) do
 
   create_table "favorites", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(:version => 20130810225550) do
     t.string   "twitter_oauth_token"
     t.string   "twitter_oauth_secret"
     t.integer  "word_count"
+    t.string   "provider"
+    t.integer  "uid"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
